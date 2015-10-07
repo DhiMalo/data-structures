e546ace8439e311bc75891ac0ecd1f0bbfecf589
DhiMalo <aherisan@gmail.com>
2015-10-07 16:07:53 -0700

    Solved for functional pattern : queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,29 @@
 var Queue = function(){
   var someInstance = {};
-
+  var index = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[index] = value;
+    index++;
   };
 
-  someInstance.dequeue = function(){
+  someInstance.dequeue = function(){ 
+    if(index > 0) {
+      var shifted = storage[0];
+      console.log('shifted is', shifted,'and storage is',storage);
+      storage[0] = storage[1];
+      // storage [1] = storage[2]
+      index--;
+    };
+    return shifted;
   };
 
   someInstance.size = function(){
+    return index;
   };
 
   return someInstance;
