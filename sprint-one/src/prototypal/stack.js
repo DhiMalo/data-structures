2c29cf0fb34b86db8edb329aa430f6073b3049c1
DhiMalo <aherisan@gmail.com>
2015-10-08 15:56:46 -0700

    Solved through prototypal : stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,25 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods); // Whisper whisper, JS compiler: I'm going to create you a nice model!  It's whatever I pass into Object.create!
+  someInstance.index = 0;
+  someInstance.storage = {};
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {}; //test suite wants to use the phrase stackMethods instead of something like Stack.prototype. Updated.
 
+stackMethods.push = function(value) {
+    this.storage[this.index] = value;
+    this.index++;
+  };
 
+stackMethods.pop = function() {
+    if (this.index > 0) {
+    var popped = this.storage[this.index-1];
+    delete this.storage[this.index-1];
+    this.index--;}
+    return popped;
+  };
+
+stackMethods.size = function() {
+  return this.index;
+};
