926cc71da7c88549f1bfdc7149ac95b7aac67b90
DhiMalo <aherisan@gmail.com>
2015-10-08 16:22:58 -0700

    Solved through prototypal : queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,26 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance.index = 0;
+  someInstance.storage =  {};
+  return someInstance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value){
+  this.storage[this.index] = value;
+  this.index++;
+};
 
+queueMethods.dequeue = function(){
+  if (this.index > 0) {
+    var shifted = this.storage[0];
+    this.storage[0] = this.storage[1];
+    this.index--;
+  }
+  return shifted; 
+};
+
+queueMethods.size = function(){
+  return this.index; 
+};
