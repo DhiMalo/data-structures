71c4b11f1888486aaf94278d25f93d6d26c4e667
DhiMalo <aherisan@gmail.com>
2015-10-08 15:21:18 -0700

    Solved for functional-shared : stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,28 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.index = 0;
+  someInstance.storage = {};
+  someInstance.push = stackMethods.push;
+  someInstance.pop = stackMethods.pop;
+  someInstance.size = stackMethods.size;
+  return someInstance;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value){
+    this.storage[this.index] = value;
+    this.index++;
+  };
 
+stackMethods.pop = function(){
+    if (this.index > 0) {
+    var popped = this.storage[this.index-1];
+    delete this.storage[this.index-1];
+    this.index--;}
+    return popped;
+  };
+
+stackMethods.size = function(){
+    return this.index;
+}
