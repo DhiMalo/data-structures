052aee4f679f8a6e9fd4c5b55bd5c4bf15e2a59d
DhiMalo <aherisan@gmail.com>
2015-10-08 14:06:33 -0700

    Removed unnecessary notes

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,28 +1,23 @@
-var Queue = function(){
+var Queue = function() {
   var someInstance = {};
   var index = 0;
-  // Use an object with numeric keys to store values
   var storage = {};
 
-  // Implement the methods below
-
-  someInstance.enqueue = function(value){
+  someInstance.enqueue = function(value) {
     storage[index] = value;
     index++;
   };
 
-  someInstance.dequeue = function(){ 
+  someInstance.dequeue = function() { 
     if(index > 0) {
       var shifted = storage[0];
-      //console.log('shifted is', shifted,'and storage is',storage);
       storage[0] = storage[1];
-      // storage [1] = storage[2]
       index--;
     };
     return shifted;
   };
 
-  someInstance.size = function(){
+  someInstance.size = function() {
     return index;
   };
 
