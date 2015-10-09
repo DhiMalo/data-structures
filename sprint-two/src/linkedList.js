c6c1935c007632ac48ff9d04535b76d7ed92c880
DhiMalo <aherisan@gmail.com>
2015-10-09 12:33:15 -0700

    Solved through new nodes

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,17 +3,34 @@ var LinkedList = function(){
   list.head = null;
   list.tail = null;
 
-  list.addToTail = function(value){
+  list.addToTail = function(value){ // thought I needed thisses but I didn't.
+    if(list.tail) { //if we've assigned a previous tail, then...
+      list.head = {};
+      list.tail; //set the head to the tail value
+      list.tail['value'] = value;
+    } else {
+      list.tail = {}; //create this cool little value storage device
+      list.tail['value'] = value; //place the value inside there
+    }
+    return value;
   };
 
   list.removeHead = function(){
+    list.head = list.tail['value'];
+    //list.head = list.tail;
   };
 
   list.contains = function(target){
+    //return (list.head === target || list.tail === target); // return bool
   };
 
   return list;
 };
+var linkedList1 = LinkedList();
+linkedList1.addToTail(4);
+//linkedList1.addToTail(5);
+//linkedList1.removeHead();
+console.log('linkedList1 is: ', linkedList1)
 
 var Node = function(value){
   var node = {};
@@ -23,6 +40,8 @@ var Node = function(value){
 
   return node;
 };
+var node1 = Node();
+console.log('node1 is: ' ,node1);
 
 /*
  * Complexity: What is the time complexity of the above functions?
