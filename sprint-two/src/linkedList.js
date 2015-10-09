49e6db5b77241ce41df466a29159d41878bb377f
DhiMalo <aherisan@gmail.com>
2015-10-09 15:43:16 -0700

    Solved for all linked list assertions

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -5,32 +5,35 @@ var LinkedList = function(){
 
   list.addToTail = function(value){ // thought I needed thisses but I didn't.
     if(list.tail) { //if we've assigned a previous tail, then...
-      list.head = {};
-      list.tail; //set the head to the tail value
       list.tail['value'] = value;
     } else {
       list.tail = {}; //create this cool little value storage device
-      list.tail['value'] = value; //place the value inside there
-    }
+      list.tail['value'] = value;
+      list.head = {};
+      list.head['value'] = list.tail['value']; //set the head to the tail value
+       //place the value inside there
+    };
     return value;
   };
 
   list.removeHead = function(){
-    list.head = list.tail['value'];
-    //list.head = list.tail;
+    var removed = list.head['value']; 
+    list.head['value'] = list.tail['value'];
+    list.tail['value'] = null;
+    return removed;
   };
 
   list.contains = function(target){
-    //return (list.head === target || list.tail === target); // return bool
+    return (list.head['value'] === target || list.tail['value'] === target); // return bool
   };
 
   return list;
 };
-var linkedList1 = LinkedList();
-linkedList1.addToTail(4);
-//linkedList1.addToTail(5);
-//linkedList1.removeHead();
-console.log('linkedList1 is: ', linkedList1)
+
+// var linkedList1 = LinkedList();
+// linkedList1.addToTail(4);
+// linkedList1.removeHead();
+// console.log('linkedList1 is: ', linkedList1)
 
 var Node = function(value){
   var node = {};
@@ -40,8 +43,9 @@ var Node = function(value){
 
   return node;
 };
-var node1 = Node();
-console.log('node1 is: ' ,node1);
+
+// var node1 = Node();
+// console.log('node1 is: ' ,node1);
 
 /*
  * Complexity: What is the time complexity of the above functions?
