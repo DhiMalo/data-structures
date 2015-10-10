9707ad69c287399a8ac5d827c224667ea80a8987
DhiMalo <aherisan@gmail.com>
2015-10-10 11:24:57 -0700

    Wrote comments on an improved implementation plan

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,19 +1,22 @@
 
+//Previously passed with 5 of 6 tests.
+//However, additional implementation notes are below.  Come back and fix these when you have time. 
 
 var Graph = function(value) {
   this.value = value;
-  this.friends = [];
+  this.friends = []; //I actually want to add the friends array to each node, not each graph.
 };
 
 Graph.prototype.addNode = function(node) {
-  var newNode = new Node(node);
-  this.friends.push(newNode);
+  var newNode = new Node(node); // So each of these newNodes needs to have a friends array (not a 'next' key like in linkedList)
+  //should create a node with value of the 'Graph'(value) above, and an empty friends array.
+  this.friends.push(newNode); //Use a nodesArray instead of a friends array?
 };
 
 Graph.prototype.contains = function(node) {
   var boolResult = false;
-  for (var i = 0; i < this.friends.length; i++) {
-    if (this.friends[i].value === node) {
+  for (var i = 0; i < this.friends.length; i++) { //iterate through the graph to see if it contains the node value
+    if (this.friends[i].value === node) { //continue on same premise here
       boolResult = true;
     };
   };
@@ -21,24 +24,24 @@ Graph.prototype.contains = function(node) {
 };
 
 Graph.prototype.removeNode = function(node){
-  for (var i = 0; i < this.friends.length; i++) {
+  for (var i = 0; i < this.friends.length; i++) { //remove from nodesArray instead of friends array.
     if (this.friends[i].value === node) {
-      delete this.friends[i].value;
+      delete this.friends[i].value; //continue on same premise here
     };
   }
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
   //set a bool to false
-  //iterate through fromNode friends.  
-  //if toNode is in its Friends Array then reset bool to TRUE!
-  //iterate through toNode friends.  
-  //if fromNode is in its Friends Array then reset bool to TRUE!
+  //iterate through fromNode edges!
+  //if toNode is in its EDGES Array then reset bool to TRUE!
+  //iterate through toNode edges.  
+  //if fromNode is in its EDGES Array then reset bool to TRUE!
   //return bool.
 
   var bool = false;
-  for (var i = 0; i < this.friends.length; i++) {
-    if (this.friends[i].value === fromNode || this.friends[i].value === toNode) {
+  for (var i = 0; i < this.edge.length; i++) { //already started changing this. Needs to make a edges array in each node, though.
+    if (this.edges[i].value === fromNode || this.edges[i].value === toNode) { //Will depend on edges Array creation.  
       bool = true;
     };
   };
@@ -48,8 +51,6 @@ Graph.prototype.hasEdge = function(fromNode, toNode){
 Graph.prototype.addEdge = function(fromNode, toNode){
   //push fromNode into toNode's friends Array.
   //push toNode into fromNode's friends Array.
-  
-
 
 };
 
@@ -59,6 +60,7 @@ Graph.prototype.removeEdge = function(fromNode, toNode){
 Graph.prototype.forEachNode = function(cb){
 };
 
+//implementation tests (remove at completion)
 var newGraph = new Graph('flowers');
 console.log(newGraph);
 newGraph.addNode('puppies');
