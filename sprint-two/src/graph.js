b23abfd56e0e8b9692a162cf07259b3bcb216a21
DhiMalo <aherisan@gmail.com>
2015-10-09 19:12:26 -0700

    Solved for two tests in graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,15 +1,31 @@
 
 
-var Graph = function(){
+var Graph = function(value) {
+  this.value = value;
+  this.friends = [];
 };
 
-Graph.prototype.addNode = function(node){
+Graph.prototype.addNode = function(node) {
+  var newNode = new Node(node);
+  this.friends.push(newNode);
 };
 
-Graph.prototype.contains = function(node){
+Graph.prototype.contains = function(node) {
+  var boolResult = false;
+  for (var i = 0; i < this.friends.length; i++) {
+    if(this.friends[i].value === node) {
+      boolResult = true;
+    };
+  };
+  return boolResult;
 };
 
 Graph.prototype.removeNode = function(node){
+  for (var i = 0; i < this.friends.length; i++) {
+    if (this.friends[i] === node) {
+      delete node;
+    };
+  }
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
@@ -23,6 +39,13 @@ Graph.prototype.removeEdge = function(fromNode, toNode){
 
 Graph.prototype.forEachNode = function(cb){
 };
+
+var newGraph = new Graph('flowers');
+console.log(newGraph);
+newGraph.addNode('puppies');
+newGraph.addNode('kittens');
+newGraph.addNode('penguins');
+
 
 /*
  * Complexity: What is the time complexity of the above functions?
