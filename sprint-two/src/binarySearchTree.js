d4f6f9e8061afe9a55a4d98a6037d7952881a3ac
MachTech <aheri@Macbook-Pro.local>
2015-10-10 16:59:32 -0700

    Passed test for binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,23 @@
 var BinarySearchTree = function(value){
+var binTree = Object.create(binarySearchMethods);
+binTree.right = [];
+binTree.left = [];
+return binTree;
 };
 
+var binarySearchMethods = {};
+
+binarySearchMethods.insert = function(value){
+
+}
+
+binarySearchMethods.contains = function(){
+  
+}
+
+binarySearchMethods.depthFirstLog = function(){
+  
+}
 
 /*
  * Complexity: What is the time complexity of the above functions?
