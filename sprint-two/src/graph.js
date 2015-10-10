a5e53063aae6a9861f2c787c6f5db823da1baf36
DhiMalo <aherisan@gmail.com>
2015-10-10 11:07:47 -0700

    Passed 5 of six tests for graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -13,7 +13,7 @@ Graph.prototype.addNode = function(node) {
 Graph.prototype.contains = function(node) {
   var boolResult = false;
   for (var i = 0; i < this.friends.length; i++) {
-    if(this.friends[i].value === node) {
+    if (this.friends[i].value === node) {
       boolResult = true;
     };
   };
@@ -22,16 +22,35 @@ Graph.prototype.contains = function(node) {
 
 Graph.prototype.removeNode = function(node){
   for (var i = 0; i < this.friends.length; i++) {
-    if (this.friends[i] === node) {
-      delete node;
+    if (this.friends[i].value === node) {
+      delete this.friends[i].value;
     };
   }
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
+  //set a bool to false
+  //iterate through fromNode friends.  
+  //if toNode is in its Friends Array then reset bool to TRUE!
+  //iterate through toNode friends.  
+  //if fromNode is in its Friends Array then reset bool to TRUE!
+  //return bool.
+
+  var bool = false;
+  for (var i = 0; i < this.friends.length; i++) {
+    if (this.friends[i].value === fromNode || this.friends[i].value === toNode) {
+      bool = true;
+    };
+  };
+  return bool;
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+  //push fromNode into toNode's friends Array.
+  //push toNode into fromNode's friends Array.
+  
+
+
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
@@ -45,6 +64,10 @@ console.log(newGraph);
 newGraph.addNode('puppies');
 newGraph.addNode('kittens');
 newGraph.addNode('penguins');
+console.log('after adding 3 nodes, newGraph is: ', newGraph);
+newGraph.removeNode('puppies');
+console.log('after removing a node, newGraph is: ', newGraph);
+
 
 
 /*
