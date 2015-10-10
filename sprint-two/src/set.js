9851771beda01cdda6ba5d0e83cd80a033f422a3
Aheri Stanford-Asiyo <aherisan@gmail.com>
2015-10-10 12:00:16 -0700

    Implemented add and contains methods for set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,27 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = [] //added array for storage
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  this._storage.push(item); //pushed item into array
 };
 
 setPrototype.contains = function(item){
+  var bool = false;
+  for (var i = 0; i < this._storage.length; i++) {
+    if (i === item) {
+      bool = true;
+    };
+  };
+  return bool;
 };
 
 setPrototype.remove = function(item){
+  
 };
 
 /*
