28fe285e0d2385e059dcca8bdec289e36da7e44a
DhiMalo <aherisan@gmail.com>
2015-10-10 14:19:48 -0700

    Code Cleanup

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,16 +1,16 @@
-var Set = function(){
+var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = [] //added array for storage
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
-setPrototype.add = function(item){
-  this._storage.push(item); //pushed item into array
+setPrototype.add = function(item) {
+  this._storage.push(item);
 };
 
-setPrototype.contains = function(item){
+setPrototype.contains = function(item) {
   var bool = false;
   for (var i = 0; i < this._storage.length; i++) {
     if (this._storage[i] === item) {
@@ -20,15 +20,18 @@ setPrototype.contains = function(item){
   return bool;
 };
 
-setPrototype.remove = function(item){
+setPrototype.remove = function(item) {
   for (var i = 0; i < this._storage.length; i++) {
     if (this._storage[i] === item) {
       var targetIndex = _.indexOf(item);
-      this._storage.splice(this._storage[targetIndex],1);
+      this._storage.splice(this._storage[targetIndex], 1);
     };
   };
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+
+The setPrototype.remove has a time complexity of O(n^2) due to the _.indexOf function needing to traverse the array after the for-loop has run.  In the worst case scenario this is n*n traversals.
  */
+ 
