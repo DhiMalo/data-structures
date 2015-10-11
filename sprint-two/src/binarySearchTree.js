664ebab218a7ef41a609955d0f46f5d8e0862c82
MachTech <aheri@Macbook-Pro.local>
2015-10-10 18:13:02 -0700

    Passed preliminary test for binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,24 +1,50 @@
-var BinarySearchTree = function(value){
+var BinarySearchTree = function(value) {
 var binTree = Object.create(binarySearchMethods);
-binTree.right = [];
-binTree.left = [];
+binTree.right = Object.create(binarySearchMethods);
+binTree.left = Object.create(binarySearchMethods);
+binTree.value = value;
 return binTree;
 };
 
 var binarySearchMethods = {};
 
-binarySearchMethods.insert = function(value){
+binarySearchMethods.insert = function(value) {
+  //return new object and pass in a new instantiation with the new value
+  if (value > this.value && !this.right.value) {
+    this.right = BinarySearchTree(value);
+    // this.right.value = value;
+    };
+  if (value > this.value && this.right.value) {
+    this.right.right = BinarySearchTree(value);
+    // this.right.right.value = value;
+  }
+  if (value < this.value && !this.left.value) {
+    this.left = BinarySearchTree(value);
+    // this.left.value = value;
+  }
+  if (value < this.value && this.left.value) {
+    this.left.left = BinarySearchTree(value);
+    // this.left.left.value = value;
+  }
+};
 
-}
-
-binarySearchMethods.contains = function(){
+binarySearchMethods.contains = function(value) {
   
-}
+};
 
-binarySearchMethods.depthFirstLog = function(){
-  
-}
+binarySearchMethods.depthFirstLog = function(callback) {
+  _.each(valuesInTree, callback)
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+binTree1 = BinarySearchTree(5)
+binTree1.insert(2);
+console.log('3 is smaller than', this.value, 'as expected?: ', 3<this.value)
+binTree1.insert(3);
+binTree1.insert(7);
+binTree1.insert(6);
+console.log(binTree1);
+console.log(binTree1.left.right.value)//3
+console.log(binTree1.right.left.value)//6
