3cfe6d55eed6ab239816c540317f2771d7bafc98
DhiMalo <aherisan@gmail.com>
2015-10-24 18:32:05 -0700

    Fixed and iterated on graph. Cleaned up code.

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,80 +1,75 @@
-
-//Previously passed with 5 of 6 tests.
-//However, additional implementation notes are below.  Come back and fix these when you have time. 
-
+/*
+ * Time complexity assessment: 
+ .addNode is Constant Time.
+ .contains and .removeNode have a Linear Time complexity.
+ .hasEdge is TBD.
+ */
 var Graph = function(value) {
   this.value = value;
-  this.friends = []; //I actually want to add the friends array to each node, not each graph.
+  this.edge = []; // every part of a graph has 2 attributes: value and edge.  It can have more than one edge.
 };
 
+// .addNode is a method that takes a new node and adds it to the graph.
+
 Graph.prototype.addNode = function(node) {
-  var newNode = new Node(node); // So each of these newNodes needs to have a friends array (not a 'next' key like in linkedList)
-  //should create a node with value of the 'Graph'(value) above, and an empty friends array.
-  this.friends.push(newNode); //Use a nodesArray instead of a friends array?
+  var newNode = new Graph(node);
+  this.edge.push(newNode); 
 };
+
+// The .contains method takes any node and returns a boolean reflecting whether it can be found in the graph.
 
 Graph.prototype.contains = function(node) {
   var boolResult = false;
-  for (var i = 0; i < this.friends.length; i++) { //iterate through the graph to see if it contains the node value
-    if (this.friends[i].value === node) { //continue on same premise here
+  for (var i = 0; i < this.edge.length; i++) { //iterate through the graph to see if it contains the node value
+    if (this.edge[i].value === node) {
       boolResult = true;
     };
   };
   return boolResult;
 };
 
-Graph.prototype.removeNode = function(node){
-  for (var i = 0; i < this.friends.length; i++) { //remove from nodesArray instead of friends array.
-    if (this.friends[i].value === node) {
-      delete this.friends[i].value; //continue on same premise here
+// .removeNode is a method that takes any node and removes it from the graph, if present. All edges connected to that Node are removed as well.
+
+Graph.prototype.removeNode = function(node) {
+  for (var i = 0; i < this.edge.length; i++) {
+    if (this.edge[i].value === node) {
+      delete this.edge[i].value;
+      // could probably also use splice here, but delete is clearer.
     };
   }
 };
 
-Graph.prototype.hasEdge = function(fromNode, toNode){
+Graph.prototype.hasEdge = function(fromNode, toNode) {
   //set a bool to false
-  //iterate through fromNode edges!
-  //if toNode is in its EDGES Array then reset bool to TRUE!
-  //iterate through toNode edges.  
-  //if fromNode is in its EDGES Array then reset bool to TRUE!
-  //return bool.
-
   var bool = false;
-  for (var i = 0; i < this.edge.length; i++) { //already started changing this. Needs to make a edges array in each node, though.
-    if (this.edges[i].value === fromNode || this.edges[i].value === toNode) { //Will depend on edges Array creation.  
+  //iterate through toNode edges.  
+    //if fromNode is in its EDGES Array then reset bool to TRUE!
+  for (var i = 0; i < this.edge.length; i++) { //
+    if (this.edge[i].value === toNode || this.edge[i].value === fromNode) { 
       bool = true;
     };
-  };
+    //if toNode is in its EDGES Array then reset bool to TRUE!
+  }
   return bool;
 };
 
-Graph.prototype.addEdge = function(fromNode, toNode){
-  //push fromNode into toNode's friends Array.
-  //push toNode into fromNode's friends Array.
-
+Graph.prototype.addEdge = function(fromNode, toNode) {
+  for (var key in this) {
+  console.log(this.key)}
+  // toNode.edge.push(fromNode);
+  // fromNode.edge.push(toNode);
 };
 
-Graph.prototype.removeEdge = function(fromNode, toNode){
-};
+Graph.prototype.removeEdge = function(fromNode, toNode) {};
 
-Graph.prototype.forEachNode = function(cb){
-};
+Graph.prototype.forEachNode = function(cb) {};
 
 //implementation tests (remove at completion)
-// var newGraph = new Graph('flowers');
-// console.log(newGraph);
-// newGraph.addNode('puppies');
-// newGraph.addNode('kittens');
-// newGraph.addNode('penguins');
-// console.log('after adding 3 nodes, newGraph is: ', newGraph);
-// newGraph.removeNode('puppies');
-// console.log('after removing a node, newGraph is: ', newGraph);
-
-
-
-/*
- * Complexity: What is the time complexity of the above functions?
- */
-
-
-
+var graph = new Graph(5);
+console.log('newly created graph is', graph);
+graph.addNode('puppies');
+graph.addNode('kittens');
+graph.addNode('penguins');
+console.log('after adding 3 nodes, graph is: ', graph);
+graph.removeNode('puppies');
+console.log('after removing a node, graph is: ', graph);
